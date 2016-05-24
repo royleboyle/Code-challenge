b8d01c11d695f2c90125bf4a6d7fd6130ea1700e
Roy Schmid <s3454624@student.rmit.edu.au>
2016-05-24 13:59:50 +1000

    RB-1 XML model completed. MFiltered by Model and Make

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @works = XmlModel.index
-    @camera_makes = XmlModel.get_camera_makes(@works)
+    @all_works = XmlModel.index
+    @camera_makes = XmlModel.get_camera_makes(@all_works)
+    @camera_models = XmlModel.get_camera_model(@all_works)
   end
 
 end
--- app/models/xml_model.rb
+++ app/models/xml_model.rb
@@ -9,13 +9,30 @@ class XmlModel < ActiveRecord::Base
   end
 
   def self.get_camera_makes(works)
-    hash = Hash.new
+    hash_camera_makes = Hash.new
     works.each do |value|
-      if (!hash.has_key? value['exif']['make'])
-        hash[value['exif']['make']] = value
+      if (!hash_camera_makes.has_key? value['exif']['make'])
+        hash_camera_makes[value['exif']['make']] = [value]
+      else
+        hash_camera_makes[value['exif']['make']].push(value)
       end
     end
-    return hash
+    return hash_camera_makes
+
+  end
+
+  def self.get_camera_model(works)
+    hash_camera_models = Hash.new
+    works.each do |value|
+      if(!hash_camera_models.has_key? value['exif']['model'])
+        hash_camera_models[value['exif']['model']] = [value]
+      else
+        hash_camera_models[value['exif']['model']].push(value)
+      end
+
+    end
+    return hash_camera_models
   end
 
 end
+
